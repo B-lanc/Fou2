40ca20630bc3c2f5cdd35d420bb6c59f2cb6ca5c
b-lanc <jerry.hatulusan.tobing@gmail.com>
2023-11-15 11:58:17 +0700

    changed how the dataset works

--- prep_hdf.py
+++ prep_hdf.py
@@ -27,12 +27,13 @@ def remove_zeros(audio, cons_zeros):
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
 def main(cfg: DictConfig) -> None:
-    if not os.path.exists(cfg.hdf_dir):
-        os.makedirs(cfg.hdf_dir)
-    vox_hdf = os.path.join(cfg.hdf_dir, "vocals.hdf5")
-    inst_hdf = os.path.join(cfg.hdf_dir, "instrumentals.hdf5")
-    vox = os.path.join(cfg.dataset_dir, "vocals", "*")
-    inst = os.path.join(cfg.dataset_dir, "instrumentals", "*")
+    hdf_dir = os.path.join(cfg.dataset_dir, "train", "hdf")
+    if not os.path.exists(hdf_dir):
+        os.makedirs(hdf_dir)
+    vox_hdf = os.path.join(hdf_dir, "vocals.hdf5")
+    inst_hdf = os.path.join(hdf_dir, "instrumentals.hdf5")
+    vox = os.path.join(cfg.dataset_dir, "train", "vocals", "*")
+    inst = os.path.join(cfg.dataset_dir, "train", "instrumentals", "*")
     vox = glob.glob(vox)
     inst = glob.glob(inst)
 
@@ -45,6 +46,25 @@ def main(cfg: DictConfig) -> None:
         for idx, track in enumerate(inst):
             song, _ = librosa.load(track, mono=True, sr=cfg.prep.sr)
             song = remove_zeros(song, cfg.prep.cons_zeros)
+            f.create_dataset(f"{idx}", data=song)
+
+    hdf_dir = os.path.join(cfg.dataset_dir, "test", "hdf")
+    if not os.path.exists(hdf_dir):
+        os.makedirs(hdf_dir)
+    vox_hdf = os.path.join(hdf_dir, "vocals.hdf5")
+    inst_hdf = os.path.join(hdf_dir, "instrumentals.hdf5")
+    vox = os.path.join(cfg.dataset_dir, "test", "vocals", "*")
+    inst = os.path.join(cfg.dataset_dir, "test", "instrumentals", "*")
+    vox = glob.glob(vox)
+    inst = glob.glob(inst)
+
+    with h5py.File(vox_hdf, "w") as f:
+        for idx, track in enumerate(vox):
+            song, _ = librosa.load(track, mono=True, sr=cfg.prep.sr)
+            f.create_dataset(f"{idx}", data=song)
+    with h5py.File(inst_hdf, "w") as f:
+        for idx, track in enumerate(inst):
+            song, _ = librosa.load(track, mono=True, sr=cfg.prep.sr)
             f.create_dataset(f"{idx}", data=song)
 
 
