ca498b6704a416dfdf976e0f9a48e66d7703dd71
b-lanc <jerry.hatulusan.tobing@gmail.com>
2023-12-23 08:38:48 +0700

    Foumou model

---
+++ Train_Foumou.py
@@ -0,0 +1,66 @@
+import torch
+import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint
+from torch.utils.data import DataLoader
+from model import Foumou
+from data.dataset import AudioHDF
+import hydra
+from omegaconf import DictConfig
+
+import os
+
+
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def main(cfg: DictConfig) -> None:
+    torch.set_float32_matmul_precision("high")
+    model = Foumou(
+        cfg.models.foumou,
+        cfg.ema,
+        cfg.lr,
+    )
+    io = model.get_io()
+    print(io)
+
+    train_set = AudioHDF(
+        os.path.join(cfg.dataset_dir, "train", "hdf", "instrumentals.hdf5"),
+        os.path.join(cfg.dataset_dir, "train", "hdf", "vocals.hdf5"),
+        input_length=io,
+        output_length=io,
+        random_hop=cfg.random_hop,
+    )
+    val_set = AudioHDF(
+        os.path.join(cfg.dataset_dir, "test", "hdf", "instrumentals.hdf5"),
+        os.path.join(cfg.dataset_dir, "test", "hdf", "vocals.hdf5"),
+        input_length=io,
+        output_length=io,
+        random_hop=False,
+    )
+
+    train_dl = DataLoader(
+        train_set, batch_size=cfg.batch_size, shuffle=True, num_workers=cfg.num_workers
+    )
+    val_dl = DataLoader(
+        val_set, batch_size=cfg.batch_size, shuffle=False, num_workers=cfg.num_workers
+    )
+    checkpoint_last = ModelCheckpoint(
+        save_top_k=3, monitor="step", mode="max", filename="last_check_{epoch:02d}"
+    )
+    checkpoint_best = ModelCheckpoint(
+        save_top_k=3, monitor="val_loss", mode="min", filename="best_check_{epoch:02d}"
+    )
+
+    save_dir = os.path.join(cfg.save_dir, cfg.tag)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    trainer = L.Trainer(
+        accelerator="gpu",
+        max_epochs=cfg.max_epochs,
+        min_epochs=cfg.min_epochs,
+        default_root_dir=save_dir,
+        callbacks=[checkpoint_best, checkpoint_last],
+    )
+    trainer.fit(model, train_dataloaders=train_dl, val_dataloaders=val_dl)
+
+
+if __name__ == "__main__":
+    main()
---
+++ model/FouMou.py
@@ -0,0 +1,154 @@
+import torch
+import torch.nn as nn
+import lightning as L
+
+from .modules import STFT, ISTFT, EMA
+
+
+crit = torch.nn.functional.l1_loss
+
+
+class CBAD(nn.Module):
+    def __init__(self, dim, trans, chin, chout, kern, stride, pad, chgn, drop):
+        super(CBAD, self).__init__()
+        if dim == 1:
+            CONV = nn.ConvTranspose1d if trans else nn.Conv1d
+        elif dim == 2:
+            CONV = nn.ConvTranspose2d if trans else nn.Conv2d
+        else:
+            raise ValueError("dim has to be either 1 or 2")
+
+        self.conv = CONV(chin, chout, kern, stride, pad)
+        self.gn = nn.GroupNorm(chgn, chout)
+        self.act = nn.ReLU()
+        self.dropout = nn.Dropout(drop)
+
+    def forward(self, x):
+        return self.dropout(self.act(self.gn(self.conv(x))))
+
+
+class ATT(nn.Module):
+    def __init__(self, ch, n_heads, drop):
+        super(ATT, self).__init__()
+        self.att = nn.MultiheadAttention(ch, n_heads, drop, batch_first=True)
+        self.ff = nn.Linear(ch, ch)
+        self.act = nn.ReLU()
+        self.dropout = nn.Dropout(drop)
+
+    def forward(self, x):
+        bs, ch, h, w = x.shape
+        x = x.reshape(bs, h * w, ch)
+        _x, _ = self.att(x, x, x)
+        x = x + _x
+        x = self.dropout(self.act(self.ff(x)))
+        x = x.reshape(bs, ch, h, w)
+        return x
+
+
+class FoUnet(nn.Module):
+    def __init__(self, cfg):
+        super(FoUnet, self).__init__()
+
+        assert len(cfg.attention_channels) == (len(cfg.channels))
+        assert len(cfg.channels) > 1
+
+        chgn = cfg.chgn
+        drop = cfg.dropout
+        kern = cfg.kernel_size
+        pad = kern // 2
+        stride = cfg.stride
+
+        self.conv_in = CBAD(2, False, 2, cfg.channels[0], 1, 1, 0, 1, drop)
+        self.encoder = nn.ModuleList()
+        self.decoder = nn.ModuleList()
+        self.conv_out = CBAD(2, False, cfg.channels[0], 2, 1, 1, 0, 1, drop)
+
+        chin = cfg.channels[0]
+        for ch, att in zip(cfg.channels, cfg.attention_channels):
+            enc = []
+            dec = []
+            enc.append(CBAD(2, False, chin, ch, 1, 1, 0, chgn, drop))
+            enc.append(CBAD(2, False, ch, ch, kern, stride, pad, chgn, drop))
+            dec.append(CBAD(2, True, ch, ch, kern, stride, pad, chgn, drop))
+            dec.append(CBAD(2, False, ch, chin, 1, 1, 0, chgn, drop))
+            for _ in range(cfg.depth):
+                enc.append(CBAD(2, False, ch, ch, kern, 1, pad, chgn, drop))
+                dec.append(CBAD(2, False, chin, chin, kern, 1, pad, chgn, drop))
+                if att:
+                    enc.append(ATT(ch, cfg.n_heads, drop))
+                    dec.append(ATT(chin, cfg.n_heads, drop))
+
+            self.encoder.append(nn.Sequential(*enc))
+            self.decoder.insert(0, nn.Sequential(*dec))
+
+            chin = ch
+
+    def forward(self, z):
+        z = self.conv_in(z)
+
+        short = [z]
+        for enc in self.encoder:
+            z = enc(z)
+            short.append(z)
+        short.reverse()
+
+        for i in range(len(self.decoder)):
+            z = z * short[i]
+            z = self.decoder[i](z)
+
+        z = z * short[-1]
+        z = self.conv_out(z)
+        return z
+
+
+class Foumou(L.LightningModule):
+    def __init__(self, cfg, ema_cfg, lr):
+        super(Foumou, self).__init__()
+        hop_size = cfg.freq - 1
+        nfft = hop_size * 2
+
+        self.io = hop_size ** 2
+        self.lr = lr
+
+        self.stft = STFT(nfft, hop_size, True, True)
+        self.istft = ISTFT(nfft, hop_size, True)
+
+        self.model = FoUnet(cfg)
+        self.ema_model = EMA(self.model, ema_cfg.beta, ema_cfg.step_start)
+
+    def get_io(self):
+        return self.io
+
+    def forward(self, x, ema=False):
+        r, i = self.stft(x)
+        r = r[:, None, :, :]
+        i = i[:, None, :, :]
+        z = torch.cat((r, i), dim=1)  # (bs, 2, nfft//2+1, nframes)
+
+        MODEL = self.ema_model if ema else self.model
+        z = MODEL(z)
+
+        r, i = z[:, 0, :, :], z[:, 1, :, :]
+        z = self.istft(r, i)
+
+        return z
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
+
+    def training_step(self, batch, batch_idx):
+        x, target = batch
+        y = self(x)
+        loss = crit(y, target)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, target = batch
+        y = self(x)
+        loss = crit(y, target)
+        self.log("val_loss", loss, on_epoch=True, prog_bar=True)
+        return loss
+
+    def on_before_zero_grad(self, optimizer):
+        self.ema_model.update_model(self.model, self.global_step)
